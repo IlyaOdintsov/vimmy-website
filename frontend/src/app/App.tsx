8a2b6285430771a9fae71a98784bd28be7371726
Ilya <run12357@yandex.ru>
2026-04-13 18:10:04 +0200

    emailJs test completed and new deploy

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,6 @@
 import AppRouter from './providers/AppRouter';
 import { Toaster } from 'sonner';
 import emailjs from '@emailjs/browser';
-import {useEffect} from "react";
 
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
@@ -14,13 +13,6 @@ emailjs.init({
 });
 
 function App() {
-	useEffect(() => {
-  		console.group('EmailJS Debug');
-  		console.log('Service ID:', import.meta.env.VITE_EMAILJS_SERVICE_ID ? '✅' : '❌');
-  		console.log('Template ID:', import.meta.env.VITE_EMAILJS_TEMPLATE_ID ? '✅' : '❌');
-  		console.log('Public Key:', import.meta.env.VITE_EMAILJS_PUBLIC_KEY ? '✅' : '❌');
-  		console.groupEnd();
-	}, []);
 	return (
 		<>
 			<AppRouter />
